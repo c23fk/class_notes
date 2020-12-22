f93419dd8ed0b63cd4f812838bb108762a1ff96e
Felix Kramarsky <student@macadmins-MacBook-2.local>
2020-12-22 12:33:04 -0500

    baseline

---
+++ list.js
@@ -0,0 +1,15 @@
+let notes = 
+[
+    //yyyy-mm-dd,class,notes
+    ["2020-12-22", 0, "the area of a triangle is 1/2bh"],
+    ["2020-12-22", 1, "electron orbitals are s p d f in that order"],
+    ["2020-12-22", 2, "read the whole poem"],
+]
+
+//classes 
+//Geo: 0
+//Chem: 1
+//Poetry: 2
+
+//Todo:
+//BINARY SEARCH
--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 
 function changeText(newString){document.getElementById("text").innerHTML = newString}
+function sub(){
+    let output
+    let chosenClass = document.getElementById("class").value;
+    let chosenDate = document.getElementById("date").value;
+    if(chosenClass == "undefined"){
+        output = "Please select a class";
+    }else{
+        output = getNote(chosenDate, chosenClass);
+    }
+    return output
+}
+function setDefaultDate(){
+    let dateString = new Date()
+    document.getElementById('date').value = `${dateString.getFullYear()}-${dateString.getMonth()+1}-${dateString.getDate()}`
+}
+
+function getNote(findDate, findClass){
+    for(let i=0; i < notes.length; i++){
+        if(findDate == notes[i][0]){
+            if(findClass == notes[i][1]){
+                return notes[i][2]
+            }
+        }
+    }
+    return "No notes for that class date pair"
+}
