84697f8cb874140ad5f662a1a89280dd1518e823
Felix Kramarsky <felixkramarsky@Felixs-MacBook-Air.local>
2020-12-25 19:17:59 -0500

    inputs

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
-function changeText(newString){document.getElementById("text").innerHTML = newString}
-function sub(){
-    let output
-    let chosenClass = document.getElementById("class").value;
-    let chosenDate = document.getElementById("date").value;
+function subOut(){
+    let output;
+    let chosenClass = document.getElementById("classOut").value;
+    let chosenDate = document.getElementById("dateOut").value;
     if(chosenClass == "undefined"){
         output = "Please select a class";
     }else{
@@ -11,9 +9,11 @@ function sub(){
     }
     return output
 }
+
 function setDefaultDate(){
     let dateString = new Date()
-    document.getElementById('date').value = `${dateString.getFullYear()}-${dateString.getMonth()+1}-${dateString.getDate()}`
+    document.getElementById('dateIn').value = `${dateString.getFullYear()}-${dateString.getMonth()+1}-${dateString.getDate()}`
+    document.getElementById('dateOut').value = `${dateString.getFullYear()}-${dateString.getMonth()+1}-${dateString.getDate()}`
 }
 
 function getNote(findDate, findClass){
@@ -29,4 +29,29 @@ function getNote(findDate, findClass){
     }
     if (output == "") {output = "No notes for that class date pair &#128078"};
     return output;
+}
+
+function subIn(){
+    let value1 = document.getElementById("dateIn").value;//day
+    let value2 = document.getElementById("classIn").value;//class
+    let note = document.getElementById("noteIn").value;//note
+    if(value2 == "undefined"){
+        document.getElementById("errorIn").innerHTML = "Please select a class."
+    }else if(note == ""){
+        document.getElementById("errorIn").innerHTML = "Please enter a note."
+    }else if(checkData(note)){
+        document.getElementById("errorIn").innerHTML = "That note already exists"
+    }else{
+        document.getElementById("errorIn").innerHTML = ""
+        notes.push([value1, value2, note]);
+    }
+}
+function checkData(checkString){
+    for(let i=0; i < notes.length; i++){
+        console.log(i)
+        if(checkString == notes[i][2]){
+            return true;
+        }
+    }
+    return false;
 }
