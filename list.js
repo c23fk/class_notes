fb2d34c5264972a684066587d703a18ffa26ad81
Felix Kramarsky <felixkramarsky@Felixs-MacBook-Air.local>
2020-12-25 22:45:58 -0500

    small stuff

--- list.js
+++ list.js
@@ -1,12 +1,6 @@
 let notes = 
 [
     //yyyy-mm-dd,class,notes
-    ["2020-12-22", 0, "The area of a triangle is 1/2bh"],
-    ["2020-12-22", 1, "Electron orbitals are s p d f in that order"],
-    ["2020-12-22", 2, "Read the whole poem"],
-    ["2020-12-22", 0, "The area of a square is s<sup>2</sup>"],
-    ["2020-12-22", 0, "I'm very cool"],
-
 ]
 
 //classes 
